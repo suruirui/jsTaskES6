26ffd08f0d782cddb0fcc1c59319fef25761fd09
suruirui <guimei201314@gmail.com>
2017-10-13 18:40:18 +0800

    ---

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry:path.resolve(__dirname,'./index.js'),
+	output:{
+		path:path.resolve(__dirname,'./build'),
+		filename:'bundle.js'
+	},
+	module:{
+		loaders:[
+			/*babel编译es6*/
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+                loaders: ['babel-loader?presets[]=es2015']
+			}
+		]
+    },
+    devServer:{  //配置服务
+        hot:true,
+        inline:true
+    }
+}
